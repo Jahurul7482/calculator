c8247795e572413141aff5cfaf6ced9bf7b1a458
jahurul7482 <sinjahurul00@gmail.com>
2022-04-26 21:14:55 +0600

    first commit

---
+++ calculator.js
@@ -0,0 +1,63 @@
+let display = document.querySelector('#display');
+
+function oneValue(){
+    display.value += 1;
+};
+function twoValue(){
+    display.value += 2;
+};
+function threeValue(){
+    display.value += 3;
+};
+function fourValue(){
+    display.value += 4;
+};
+function fiveValue(){
+    display.value += 5;
+};
+function sixValue(){
+    display.value += 6;
+};
+function sevenValue(){
+    display.value += 7;
+};
+function eightValue(){
+    display.value += 8;
+};
+function nineValue(){
+    display.value += 9;
+};
+function zeroValue(){
+    display.value += 0;
+};
+function doubleZeroValue(){
+    display.value += "00";
+};
+function delOneDeg(){
+    display.value = "";
+};
+function powerOn(){
+    display.value = "0";
+}
+function devidedValue(){
+    display.value += '/';
+};
+function percentValue(){
+    display.value += "%";
+};
+function maltiplerValue(){
+    display.value += "*";
+};
+function minusValue(){
+    display.value += "-";
+};
+function plusValue(){
+    display.value += "+";
+};
+function dotValue(){
+    display.value += ".";
+};
+
+function evalValue(){
+    display.value = eval(display.value);
+};
